2daa92c7ffef95e722e84137c248662962906590
eonuchkova <e.onuchkova@overgear.com>
2025-12-07 14:31:07 +0300

    Испраленное задание 22

--- src/homework22/MultiplicationTable.java
+++ src/homework22/MultiplicationTable.java
@@ -6,35 +6,60 @@ public class MultiplicationTable {
         return String.valueOf(number).length();
     }
 
+    public static int doubleSearch(Scanner scanner) throws Exception {
+        String s = scanner.next();
+        try {
+            return Integer.parseInt(s);
+        } catch (NumberFormatException e) {
+            throw new Exception("Ошибка: введите целое число");
+        }
+    }
+
+    public static void printTable(int[] values, int cellWidth) {
+        System.out.print(" ".repeat(cellWidth));
+        for (int v : values) System.out.printf("%" + cellWidth + "d", v);
+        System.out.println();
+
+        for (int x : values) {
+            System.out.printf("%" + cellWidth + "d", x);
+            for (int y : values) System.out.printf("%" + cellWidth + "d", x * y);
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int firstNumber = 0;
+        int secondNumber = 0;
+        int step = 0;
 
-        System.out.print("Введите первое число: ");
-        int firstNumber = scanner.nextInt();
+        try {
+            System.out.print("Введите первое число: ");
+            firstNumber = doubleSearch(scanner);
 
-        System.out.print("Введите второе число: ");
-        int secondNumber = scanner.nextInt();
+            System.out.print("Введите второе число: ");
+            secondNumber = doubleSearch(scanner);
 
-        System.out.print("Введите шаг: ");
-        int step = scanner.nextInt();
+            System.out.print("Введите шаг: ");
+            step = doubleSearch(scanner);
 
-        if (firstNumber > secondNumber){
-        System.out.print("Ошибка: первое число должно быть меньше второго");
-        return;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return;
         }
 
-        if (step < 0){
+
+        if (firstNumber > secondNumber) {
+            System.out.print("Ошибка: первое число должно быть меньше второго");
+            return;
+        }
+
+        if (step < 0) {
             System.out.print("Ошибка: шаг должен быть больше 0");
             return;
         }
 
-        //Не понимаю, как добавить проверку на целое число, чтобы при вводе дробного числа выводилось сообщение "Ошибка: введите целое число".
-//        if (firstNumber = double firstNumber){
-//            System.out.print("Ошибка: введите целое число");
-//            return;
-//        }
-
-            int count = ((secondNumber - firstNumber) / step) + 1;
+        int count = ((secondNumber - firstNumber) / step) + 1;
         if (firstNumber + step * (count - 1) != secondNumber) count++;
 
         int[] values = new int[count];
@@ -50,15 +75,7 @@ public class MultiplicationTable {
                 if (w > maxWidth) maxWidth = w;
             }
         }
-
-        System.out.print(" ".repeat(maxWidth + 1));
-        for (int v : values) System.out.printf("%" + (maxWidth + 1) + "d", v);
-        System.out.println();
-
-        for (int x : values) {
-            System.out.printf("%" + (maxWidth + 1) + "d", x);
-            for (int y : values) System.out.printf("%" + (maxWidth + 1) + "d", x * y);
-            System.out.println();
-        }
+        int cellWidth = maxWidth + 1;
+        printTable(values, cellWidth);
     }
 }
